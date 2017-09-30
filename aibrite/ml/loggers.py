8fc93322e86d7e832422c5a29d2ac8b9344ab542
theutku <turkoglu.utku@gmail.com>
2017-09-30 20:54:16 +0300

    .

--- aibrite/ml/loggers.py
+++ aibrite/ml/loggers.py
@@ -46,7 +46,8 @@ class MongodbLogger(AnalyserLoggerBase):
         self.collections = {
             'session': self.db.session,
             'train': self.db.train,
-            'prediction': self.db.prediction
+            'prediction': self.db.prediction,
+            'classifier': self.db.classifier
         }
 
     def create_session():
@@ -59,9 +60,44 @@ class MongodbLogger(AnalyserLoggerBase):
         }
 
         try:
-            inserted_session = self.collections['session'].insert_one(data)
+            inserted_session = self.collections.session.insert_one(data)
             self.session_id = inserted_session.inserted_id
             return self.session_id
+        except Exception as e:
+            print(str(e.args))
+
+    def add_to_train_log(self, neuralnet, train_data, prediction=None, extra_data=None):
+        extra_data = extra_data if extra_data != None else {}
+        hyper_parameters = neuralnet.get_hyperparameters()
+        now = datetime.datetime.now()
+        if prediction is None:
+            prediction_data = {}
+        else:
+            test_set_id, prediction_result = prediction
+            precision, recall, f1, support = prediction_result.score.totals
+            prediction_data = {
+                'test_set': test_set_id,
+                'precision': precision,
+                'recall': recall,
+                'accuracy': prediction_result.score.accuracy,
+                'f1': f1,
+                'support': support,
+            }
+
+        data = {
+            'timestamp': now,
+            'session_id': self.session_id,
+            'session_name': self.analyser.session_name,
+            'classifier': neuralnet.__class__.__name__,
+            'classifier_instance': neuralnet.instance_id,
+            'extra_data': extra_data,
+            'train_data': train_data,
+            'prediction': prediction_data,
+        }
+
+        try:
+            inserted_train = self.collections.session.insert_one(data)
+            return inserted_train.inserted_id
         except Exception as e:
             print(str(e.args))
 
