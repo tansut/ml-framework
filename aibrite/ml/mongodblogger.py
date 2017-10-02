f06c43c22187a40f958d1f23b0c81d318dbf2402
Tansu TURKOGLU <tansut@gmail.com>
2017-10-02 10:43:09 +0300

    removed mongodblogger dependency

---
+++ aibrite/ml/mongodblogger.py
@@ -0,0 +1,138 @@
+from aibrite.ml.loggers import AnalyserLoggerBase
+
+import os
+import datetime
+import re
+from threading import Lock
+import pandas as pd
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+
+class MongodbLogger(AnalyserLoggerBase):
+
+    def __init__(self, analyser, conn_str='mongodb://localhost:27017'):
+        super().__init__(analyser)
+        self.conn_str = conn_str
+        self.client = MongoClient(conn_str)
+
+        self.db = self.client.nn_sandbox
+        self.collections = {
+            'session': self.db.session,
+            'train': self.db.train,
+            'prediction': self.db.prediction,
+            'classifier': self.db.classifier
+        }
+
+    def create_session(self):
+        analyser = self.analyser
+        data = {
+            'session_name': analyser.session_name,
+            'group_name': analyser.group,
+            'timestamp': datetime.datetime.now(),
+            'status': 'created'
+        }
+
+        try:
+            inserted_session = self.collections.session.insert_one(data)
+            self.session_id = inserted_session.inserted_id
+            return self.session_id
+        except Exception as e:
+            print(str(e.args))
+
+    def add_to_train_log(self, neuralnet, train_data, prediction=None, extra_data=None):
+        extra_data = extra_data if extra_data != None else {}
+        hyper_parameters = neuralnet.get_hyperparameters()
+        now = datetime.datetime.now()
+        if prediction is None:
+            prediction_data = {}
+        else:
+            test_set_id, prediction_result = prediction
+            precision, recall, f1, support = prediction_result.score.totals
+            prediction_data = {
+                'test_set': test_set_id,
+                'precision': precision,
+                'recall': recall,
+                'accuracy': prediction_result.score.accuracy,
+                'f1': f1,
+                'support': support,
+            }
+
+        data = {
+            'timestamp': now,
+            'session_id': self.session_id,
+            'session_name': self.analyser.session_name,
+            'classifier': neuralnet.__class__.__name__,
+            'classifier_instance': neuralnet.instance_id,
+            'extra_data': extra_data,
+            'train_data': train_data,
+            'prediction': prediction_data
+        }
+
+        try:
+            inserted_train = self.collections.session.insert_one(data)
+            return inserted_train.inserted_id
+        except Exception as e:
+            print(str(e.args))
+
+    def add_to_classifier_instances(self, neuralnet):
+        hyper_parameters = neuralnet.get_hyperparameters()
+        data = {
+            'session': self.session_id,
+            'classifier': neuralnet.__class__.__name__,
+            'classifier_instance': neuralnet.instance_id,
+            'hyperparameters': hyper_parameters
+        }
+        try:
+            inserted_classifier = self.collections.classifier.insert_one(data)
+            return inserted_classifier.inserted_id
+        except Exception as e:
+            print(str(e.args))
+
+    def add_to_prediction_log(self, neuralnet, test_set_id, prediction_result, extra_data=None):
+        extra_data = extra_data if extra_data != None else {}
+        score = prediction_result.score
+        precision, recall, f1, support = score.totals
+        hyper_parameters = neuralnet.get_hyperparameters()
+        now = datetime.datetime.now()
+
+        data = {
+            'timestamp': now,
+            'classifier': neuralnet.__class__.__name__,
+            'test_set': test_set_id,
+            'precision': precision,
+            'recall': recall,
+            'accuracy': score.accuracy,
+            'f1': f1,
+            'support': support,
+            'label': '__totals__',
+            'classifier_instance': neuralnet.instance_id,
+            'prediction_time': prediction_result.elapsed,
+            'train_time': neuralnet.train_result.elapsed,
+            'session_name': self.analyser.session_name
+        }
+
+        try:
+            inserted_prediction = self.collections.prediction.insert_one(data)
+            return inserted_prediction.inserted_id
+        except Exception as e:
+            print(str(e.args))
+
+    def update_session(self, values):
+        # status = values.status
+        # update_value = {
+        #     'status': status
+        # }
+        try:
+            self.collections.session.update_one({
+                '_id': self.session_id,
+                '$set': values
+            })
+        except Exception as e:
+            print(str(e.args))
+
+    def init(self):
+        pass
+
+    def done(self):
+        pass
---
+++ multi.process.demo.py
@@ -0,0 +1,88 @@
+import concurrent.futures
+import time
+
+import pandas as pd
+import numpy as np
+
+from aibrite.ml.neuralnet import NeuralNet
+from aibrite.ml.neuralnetwithmomentum import NeuralNetWithMomentum
+from aibrite.ml.neuralnetwithrmsprop import NeuralNetWithRMSprop
+from aibrite.ml.neuralnetwithadam import NeuralNetWithAdam
+from aibrite.ml.analyser import NeuralNetAnalyser
+from aibrite.ml.loggers import CsvLogger
+
+
+df = pd.read_csv("./data/winequality-red.csv", sep=";")
+
+# df = df[df['quality'] != 8.0]
+# df = df[df['quality'] != 3.0]
+
+np.random.seed(5)
+data = df.values
+
+train_set, test_set, dev_set = NeuralNet.split(
+    data, 0.6, 0.20, 0.20, shuffle=True)
+
+train_x, train_y = (train_set[:, 0:-1]), train_set[:, -1]
+dev_x, dev_y = (dev_set[:, 0:-1]), dev_set[:, -1]
+test_x, test_y = (test_set[:, 0:-1]), test_set[:, -1]
+
+labels = [3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
+
+normalize_inputs = [True]
+iteration_count = [300, 600, 900]
+learning_rate = [0.005, 0.003]
+hidden_layers = [(12, 6, 24, 12)]
+lambds = [0.2, 0.4]
+learnin_rate_decay = [0.5]
+epoch = [1, 2]
+shuffle = [True]
+minibatch_size = [0]
+
+test_sets = {'dev_set': (dev_x, dev_y),
+             'test_set': (test_x, test_y),
+             'train_set': (train_x, train_y)}
+test_sets = {'train_set': (train_x, train_y)}
+# test_sets = {'test_set': (test_x, test_y)}
+
+
+def jb(analyser, results):
+    pass
+
+
+# logger = CsvLogger("Coursera Ex2 Analysis", overwrite=True,
+#                    base_dir='./analyserlogs')
+
+analyser = NeuralNetAnalyser(
+    "Coursera Ex2 Analysis", job_completed=jb)
+
+train_set = (train_x, train_y)
+
+for it in iteration_count:
+    for lr in learning_rate:
+        for hl in hidden_layers:
+            for lambd in lambds:
+                for ni in normalize_inputs:
+                    for lrd in learnin_rate_decay:
+                        for ep in epoch:
+                            for shl in shuffle:
+                                for mbs in minibatch_size:
+                                    analyser.submit(NeuralNetWithAdam, train_set, test_sets,
+                                                    hidden_layers=hl,
+                                                    learning_rate=lr,
+                                                    learning_rate_decay=lrd,
+                                                    iteration_count=it,
+                                                    lambd=lambd,
+                                                    normalize_inputs=ni,
+                                                    epochs=ep,
+                                                    shuffle=shl,
+                                                    minibatch_size=mbs,
+                                                    labels=labels)
+
+analyser.join()
+
+if len(test_sets) > 1:
+    selected = analyser.get_testset_from_user()
+    analyser.print_summary(selected)
+else:
+    analyser.print_summary()
